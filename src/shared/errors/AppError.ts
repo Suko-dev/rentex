a6620b3d9345d7290311c8f017f6f592cc4c024f
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-26 12:28:00 -0300

    feat: created new error class

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,11 @@
+export class AppError extends Error {
+    public readonly message: string;
+
+    public readonly status: number;
+
+    constructor(message: string, status = 400) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+}
