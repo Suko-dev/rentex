97b859e28eb6bd2d48ec31799ba0edd7e6dd55c6
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-26 12:55:55 -0300

    feat: add findbyid and list to ICategoryRepository

--- src/modules/Categories/infra/ICategoryRepository.ts
+++ src/modules/Categories/infra/ICategoryRepository.ts
@@ -4,6 +4,8 @@ import { Category } from "./typeorm/entities/Category";
 interface ICategoryRepository {
     create(data: ICreateCategoryDTO): Promise<void>;
     findByName(name: string): Category | undefined;
+    findById(id: string): Category | undefined;
+    list(): Category[];
 }
 
 export { ICategoryRepository };
