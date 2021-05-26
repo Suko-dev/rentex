5afcf175a0704f013ee50d6dc7897f48faab0db4
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-26 19:03:37 -0300

    [feat]: create FindCategoryByNameUseCase
    Created method for FindCategoryByNameUseCase

---
+++ src/modules/Categories/useCase/FindCategoryByName/FindCategoryByNameUseCase.ts
@@ -0,0 +1,12 @@
+import { ICategoryRepository } from "@modules/Categories/infra/ICategoryRepository";
+import { Category } from "@modules/Categories/infra/typeorm/entities/Category";
+
+class FindCategoryByNameUseCase {
+    constructor(private categoryRepository: ICategoryRepository) {}
+
+    async execute(name: string): Promise<Category | undefined> {
+        return this.categoryRepository.findByName(name);
+    }
+}
+
+export { FindCategoryByNameUseCase };
