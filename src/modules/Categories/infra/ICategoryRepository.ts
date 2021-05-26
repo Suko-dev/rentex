fb0febdf4dc14614652cf236939c2fd2c1e1c19b
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-26 10:46:28 -0300

    feat: add findbyname on ICategoryRepository

--- src/modules/Categories/infra/ICategoryRepository.ts
+++ src/modules/Categories/infra/ICategoryRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateCategoryDTO } from "../dtos/ICreateCategoryDTO";
+import { Category } from "./typeorm/entities/Category";
 
 interface ICategoryRepository {
     create(data: ICreateCategoryDTO): Promise<void>;
+    findByName(name: string): Category | undefined;
 }
 
 export { ICategoryRepository };
