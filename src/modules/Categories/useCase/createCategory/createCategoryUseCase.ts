ea522642968a35eec84fe08b4449dc7c36548e9c
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-26 00:00:31 -0300

    feat: implemented createCategoryUseCase

--- src/modules/Categories/useCase/createCategory/createCategoryUseCase.ts
+++ src/modules/Categories/useCase/createCategory/createCategoryUseCase.ts
@@ -1,6 +1,11 @@
+import { ICreateCategoryDTO } from "@modules/Categories/dtos/ICreateCategoryDTO";
+import { ICategoryRepository } from "@modules/Categories/infra/ICategoryRepository";
+
 class CreateCategoryUseCase {
-    async execute(): Promise<void> {
-        console.log("eu crio uma categoria");
+    constructor(private categoryRepository: ICategoryRepository) {}
+
+    async execute({ name, description }: ICreateCategoryDTO): Promise<void> {
+        this.categoryRepository.create({ name, description });
     }
 }
 
