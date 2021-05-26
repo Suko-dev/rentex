908937b8f710f7d43c9a93f43dae25c828514991
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-26 12:29:02 -0300

    feat: implements bussines rule  createcategorycase

--- src/modules/Categories/useCase/createCategory/createCategoryUseCase.ts
+++ src/modules/Categories/useCase/createCategory/createCategoryUseCase.ts
@@ -1,10 +1,16 @@
 import { ICreateCategoryDTO } from "@modules/Categories/dtos/ICreateCategoryDTO";
 import { ICategoryRepository } from "@modules/Categories/infra/ICategoryRepository";
 
+import { AppError } from "../../../../shared/errors/AppError";
+
 class CreateCategoryUseCase {
     constructor(private categoryRepository: ICategoryRepository) {}
 
     async execute({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const categoryAlreadyExist = this.categoryRepository.findByName(name);
+        if (categoryAlreadyExist) {
+            throw new AppError("Category already exists");
+        }
         this.categoryRepository.create({ name, description });
     }
 }
