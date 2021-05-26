3728c91a6fcf3ae14c29db00d6108e8b41ff02af
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-26 10:45:52 -0300

    feat: add test for createCategoryUseCase

--- src/modules/Categories/useCase/createCategory/createCategoryUseCase.spec.ts
+++ src/modules/Categories/useCase/createCategory/createCategoryUseCase.spec.ts
@@ -1,12 +1,22 @@
+import { CategoryRepositoryInMemory } from "../../infra/in-memory/CategoryRepositoryInMemory";
 import { CreateCategoryUseCase } from "./createCategoryUseCase";
 
 let createCategoryUseCase: CreateCategoryUseCase;
+let categoryRepositoryInMemory: CategoryRepositoryInMemory;
 
 describe("Criar categoria", () => {
     beforeEach(() => {
-        createCategoryUseCase = new CreateCategoryUseCase();
+        categoryRepositoryInMemory = new CategoryRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            categoryRepositoryInMemory
+        );
     });
     it("should be able to create a new category", async () => {
-        await createCategoryUseCase.execute();
+        await createCategoryUseCase.execute({
+            name: "suv",
+            description: "categoria suv",
+        });
+        const category = await categoryRepositoryInMemory.findByName("suv");
+        expect(category).toHaveProperty("id");
     });
 });
