6918ff62b15f5971fbee46878485e4ca203f3bd0
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-31 18:35:24 -0300

    [refactor]: changed server
    split server in server and app for more clear responsabilities

---
+++ src/shared/http/app.ts
@@ -0,0 +1,5 @@
+import { app } from "./server";
+
+app.listen(8080, async () => {
+    console.log(`Servidor rodando na porta 8080`);
+});
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -11,6 +11,3 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 connection("database");
 
-app.listen(8080, async () => {
-    console.log(`Servidor rodando na porta 8080`);
-});
