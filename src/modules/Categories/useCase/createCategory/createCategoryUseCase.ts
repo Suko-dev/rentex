26e2fddb6c780048d7a193149dc400fcc9174772
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-31 18:32:05 -0300

    [refactor]: changed CreateCategoryUseCase
    Altered CreateCategoryUseCase to accept injection from repository

--- src/modules/Categories/useCase/createCategory/createCategoryUseCase.ts
+++ src/modules/Categories/useCase/createCategory/createCategoryUseCase.ts
@@ -1,13 +1,20 @@
 import { ICreateCategoryDTO } from "@modules/Categories/dtos/ICreateCategoryDTO";
 import { ICategoryRepository } from "@modules/Categories/infra/ICategoryRepository";
+import { inject, injectable } from "tsyringe";
 
 import { AppError } from "@shared/errors/AppError";
 
+@injectable()
 class CreateCategoryUseCase {
-    constructor(private categoryRepository: ICategoryRepository) {}
+    constructor(
+        @inject("CategoryRepository")
+        private categoryRepository: ICategoryRepository
+    ) {}
 
     async execute({ name, description }: ICreateCategoryDTO): Promise<void> {
-        const categoryAlreadyExist = this.categoryRepository.findByName(name);
+        const categoryAlreadyExist = await this.categoryRepository.findByName(
+            name
+        );
         if (categoryAlreadyExist) {
             throw new AppError("Category already exists");
         }
