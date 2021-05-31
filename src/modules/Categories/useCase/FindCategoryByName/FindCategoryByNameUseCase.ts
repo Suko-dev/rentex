c81c868161e8d4897206b2b73551d3216b6d95b3
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-31 19:46:51 -0300

    [refactor]: inject repository
    injected category repository on FindCategoryByNameUseCase

--- src/modules/Categories/useCase/FindCategoryByName/FindCategoryByNameUseCase.ts
+++ src/modules/Categories/useCase/FindCategoryByName/FindCategoryByNameUseCase.ts
@@ -1,8 +1,13 @@
 import { ICategoryRepository } from "@modules/Categories/infra/ICategoryRepository";
 import { Category } from "@modules/Categories/infra/typeorm/entity/Category";
+import { inject, injectable } from "tsyringe";
 
+@injectable()
 class FindCategoryByNameUseCase {
-    constructor(private categoryRepository: ICategoryRepository) {}
+    constructor(
+        @inject("CategoryRepository")
+        private categoryRepository: ICategoryRepository
+    ) {}
 
     async execute(name: string): Promise<Category | undefined> {
         return this.categoryRepository.findByName(name);
