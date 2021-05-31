85d3f7c9a37798230ef820f389c6f10f69b14126
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-05-31 19:50:36 -0300

    [feat]: add route
    added a category route for ListCategoryController

--- src/shared/http/routes/category.routes.ts
+++ src/shared/http/routes/category.routes.ts
@@ -1,12 +1,15 @@
 import { CreateCategoryController } from "@modules/Categories/useCase/createCategory/createCategoryController";
 import { FindCategoryByNameController } from "@modules/Categories/useCase/FindCategoryByName/findCategoryByNameController";
+import { ListCategoryController } from "@modules/Categories/useCase/listCategory/listCategoryController";
 import { Router } from "express";
 
 const categoriesRoutes = Router();
 const createCategoryController = new CreateCategoryController();
 const findCategoryByNameController = new FindCategoryByNameController();
+const listCategoryController = new ListCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 categoriesRoutes.get("/:category", findCategoryByNameController.handle);
+categoriesRoutes.get("/", listCategoryController.handle);
 
 export { categoriesRoutes };
