dd18d974e237be0bca3534950a982f89c12de890
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-01 17:56:41 -0300

    [feat]: create container
    created container for user repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,5 @@
+import { IUserRepository } from "@modules/Account/infra/IUserRepository";
+import { UserRepository } from "@modules/Account/infra/typeorm/repositories/UserRepository";
 import { ICategoryRepository } from "@modules/Categories/infra/ICategoryRepository";
 import { CategoryRepository } from "@modules/Categories/infra/typeorm/repositories/categoryRepository";
 import { container } from "tsyringe";
@@ -6,3 +8,5 @@ container.registerSingleton<ICategoryRepository>(
     "CategoryRepository",
     CategoryRepository
 );
+
+container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
