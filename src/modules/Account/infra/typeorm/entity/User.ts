6110b253c0df2645b2cfeb25e3d8378a983f2a18
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-01 12:51:25 -0300

    [feat]:create entity
    Created entity for user

---
+++ src/modules/Account/infra/typeorm/entity/User.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+
+class User {
+    id: string;
+
+    name: string;
+
+    username: string;
+
+    password: string;
+
+    email: string;
+
+    driver_license: string;
+
+    admin: boolean;
+
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4();
+        }
+    }
+}
+
+export { User };
