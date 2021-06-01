2227279c57dd082e663c0e43a0d97efce9da70a2
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-01 18:00:36 -0300

    [feat]: route
    added user routes do router index

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { categoriesRoutes } from "@shared/http/routes/category.routes";
+import { userRoutes } from "@shared/http/routes/user.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
+router.use("/user", userRoutes);
 
 export { router };
