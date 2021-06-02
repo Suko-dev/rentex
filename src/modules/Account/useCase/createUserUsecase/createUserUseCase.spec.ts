6a90d2f0b3f6063d2fd04cdbeb2dab84fd20030d
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-01 23:01:34 -0300

    [test]: implement
    implementent missing tests for some createUserUseCase bussines rules

--- src/modules/Account/useCase/createUserUsecase/createUserUseCase.spec.ts
+++ src/modules/Account/useCase/createUserUsecase/createUserUseCase.spec.ts
@@ -21,4 +21,40 @@ describe("Create user", () => {
         const user = await userRepositoryInMemory.findByEmail("joe@email.com");
         expect(user).toHaveProperty("id");
     });
+    it("should not be able to create a new user with an existing username", async () => {
+        expect(async () => {
+            await createUserUseCase.execute({
+                name: "joe",
+                username: "doe",
+                password: "123",
+                email: "joe@email.com",
+                driver_license: "1234",
+            });
+            await createUserUseCase.execute({
+                name: "joe",
+                username: "doe",
+                password: "123",
+                email: "joe1@email.com",
+                driver_license: "1234",
+            });
+        }).rejects.toThrow("Username already registered");
+    });
+    it("should not be able to create a new user with an existing email", async () => {
+        expect(async () => {
+            await createUserUseCase.execute({
+                name: "joe",
+                username: "doe",
+                password: "123",
+                email: "joe@email.com",
+                driver_license: "1234",
+            });
+            await createUserUseCase.execute({
+                name: "joe",
+                username: "doe1",
+                password: "123",
+                email: "joe@email.com",
+                driver_license: "1234",
+            });
+        }).rejects.toThrow("Email already registered");
+    });
 });
